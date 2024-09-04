e7f30f912873c914b37dd8f440becb7a2afdb2c6
MateusJeanfelice <128821478+MateusJeanfelice@users.noreply.github.com>
2024-09-04 08:01:00 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 export const perguntas = [
     {
-        enunciado: " ",
+        enunciado: "Vocẽ quer comprar uma moto nova, e esta na dúvida entre a moto dos seus sonhos ou uma moto mais adequada para seu uso. Qual você escolheria ? ",
         alternativas: [
             {
-                texto: "",
+                texto: "A moto dos sonhos",
                 afirmacao: [
-                    "",
+                    "Realizar seus sonhos motiva a conquistar coisas cada vez melhores",
                     ""
                 ],
                 proxima: 1,
             },
             {
-                texto: "",
+                texto: "A mais adequada",
                 afirmacao: [
-                    "",
+                    "A escolha mais racional visando usabilidade e economia sem dúvidas é uma ótima ideia",
                     ""
                 ],
                 proxima: 2,
@@ -21,66 +21,20 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Cidades grandes têm um alto índice de criminalidade e seria melhor proteger sua moto de alguma forma como:",
         alternativas: [
             {
-                texto: "",
+                texto: "Contratar uma seguradora",
                 afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 3,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 4,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 3,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 4,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
+                    "Seguradoras são a garantia que vocẽ não ira perder o valor investido no veículo em casos de roubo ou acidentes",
                     ""
                 ],
                 proxima: 5,
             },
             {
-                texto: "",
+                texto: "Instalar um rastreador",
                 afirmacao: [
-                    "",
+                    "Rastreadores são muito baratos e ajudam caso um veículo seja roubado ajudando-o a localizar",
                     ""
                 ],
                 proxima: 6,
@@ -88,86 +42,26 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Ao comprar um veículo usado pode conter problemas que você não espera, podendo causar alguns transtornos. Você optaria em:",
         alternativas: [
             {
-                texto: "",
+                texto: "Levar a uma oficina e fazer uma revisão",
                 afirmacao: [
+                    "Manter o veículo sempre revisado vai te tirar de problemas que podem acontecer e aumenta sua segurança",
                     "",
                     ""
                 ],
-                proxima: 5,
+                proxima: 3,
             },
             {
-                texto: "",
+                texto: "Usar até apresentar algum problema",
                 afirmacao: [
+                    "Não ira fazer você gastar muito dinheiro de ma vez com uma revisão mas poderá te deixar na mão por algum defeito",
                     "",
                     ""
                 ],
-                proxima: 6,
+                proxima: 4,
             },
         ]
     },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-            },
-        ]
-    }
-];
+   
